d405e595fd70def21060a47a7ac453aa9143ea81
Simon Michael <simon@joyful.com>
2008-10-16 11:53:50 +0200

    drop --usage flag
    Ignore-this: 2aa43f90fddcc12d2d092ee54bf82da4
    
    darcs-hash:20081016095350-3c3f9-cd45c95f797b41d27899e3f10c6cce5b3b570543.gz

--- Options.hs
+++ Options.hs
@@ -28,7 +28,7 @@ options = [
  Option ['C'] ["cleared"]      (NoArg  Cleared)            "report only on cleared entries",
  Option ['R'] ["real"]         (NoArg  Real)               "report only on real (non-virtual) transactions",
  Option ['s'] ["showsubs"]     (NoArg  ShowSubs)           "in the balance report, include subaccounts",
- Option ['h'] ["help","usage"] (NoArg  Help)               "show this help",
+ Option ['h'] ["help"] (NoArg  Help)                       "show this help",
  Option ['V'] ["version"]      (NoArg  Version)            "show version"
  ]
 
