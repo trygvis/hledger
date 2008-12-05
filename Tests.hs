6efba8c1a4e383954fd02380b0219f6243dfc1d9
Simon Michael <simon@joyful.com>
2008-12-05 10:28:29 +0100

    test
    Ignore-this: 51a15f4e46369e518a2f40d12a63e7a0
    
    darcs-hash:20081205092829-3c3f9-85fbd976c59a76fe89ef96fb590ce4ec9f4d4cce.gz

--- Tests.hs
+++ Tests.hs
@@ -440,6 +440,7 @@ registercommand_tests = TestList [
     "2008" `periodexprgives` ["2008/01/01","2008/06/01","2008/06/02","2008/06/03","2008/12/31"]
     "2007" `periodexprgives` []
     "june" `periodexprgives` ["2008/06/01","2008/06/02","2008/06/03"]
+    "monthly" `periodexprgives` ["2008/01/01","2008/06/01","2008/12/01"]
 
     assertequal (
      "2008/01/01 - 2008/12/31         assets:bank:saving               $1           $1\n" ++
