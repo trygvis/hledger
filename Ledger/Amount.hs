747fc344096180e394cadc80ddc7fc768157ca71
Simon Michael <simon@joyful.com>
2009-11-25 13:57:30 +0100

    docs: haddock fixes
    Ignore-this: 56fd5370842efa1b268cf6b084de569b
    
    darcs-hash:20091125125730-3c3f9-deb846ea7e7d3be180f0eddad26fd1fc637a97c7.gz

--- Ledger/Amount.hs
+++ Ledger/Amount.hs
@@ -106,7 +106,7 @@ showAmount a@(Amount (Commodity {symbol=sym,side=side,spaced=spaced}) _ pri) =
       price = case pri of (Just pamt) -> " @ " ++ showMixedAmount pamt
                           Nothing -> ""
 
--- | Get the string representation of an amount, without any "@ price".
+-- | Get the string representation of an amount, without any \@ price.
 showAmountWithoutPrice :: Amount -> String
 showAmountWithoutPrice a = showAmount a{price=Nothing}
 
@@ -170,7 +170,7 @@ showMixedAmount m = concat $ intersperse "\n" $ map showfixedwidth as
       showfixedwidth = printf (printf "%%%ds" width) . show
 
 -- | Get the string representation of a mixed amount, but without
--- any "@ price"s.
+-- any \@ prices.
 showMixedAmountWithoutPrice :: MixedAmount -> String
 showMixedAmountWithoutPrice m = concat $ intersperse "\n" $ map showfixedwidth as
     where
@@ -186,7 +186,7 @@ showMixedAmountOrZero a
     | otherwise = showMixedAmount a
 
 -- | Get the string representation of a mixed amount, or a bare 0,
--- without any "@ price"s.
+-- without any \@ prices.
 showMixedAmountOrZeroWithoutPrice :: MixedAmount -> String
 showMixedAmountOrZeroWithoutPrice a
     | isZeroMixedAmount a = "0"
