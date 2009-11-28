a64b35ec7896330398985fd852d6acfdd1c39f84
Simon Michael <simon@joyful.com>
2009-11-28 16:25:51 +0100

    refactor
    Ignore-this: 9acb42db6b56b3962a0a64bae96cbc1f
    
    darcs-hash:20091128152551-3c3f9-9afd2d96753e1fa3eaada6d2869341b6d7f05eb7.gz

--- Commands/Register.hs
+++ Commands/Register.hs
@@ -34,7 +34,7 @@ showRegisterReport opts args l
     | otherwise = showtxns summaryts nulltxn startbal
     where
       interval = intervalFromOpts opts
-      ts = sortBy (comparing tdate) $ filterempties $ filter matchapats $ filterdepth $ ledgerTransactions l
+      ts = sortBy (comparing tdate) $ filterempties $ filtertxns apats $ filterdepth $ ledgerTransactions l
       filterdepth | interval == NoInterval = filter (\t -> accountNameLevel (taccount t) <= depth)
                   | otherwise = id
       filterempties
@@ -43,7 +43,6 @@ showRegisterReport opts args l
       (precedingts, ts') = break (matchdisplayopt dopt) ts
       (displayedts, _) = span (matchdisplayopt dopt) ts'
       startbal = sumTransactions precedingts
-      matchapats = matchpats apats . taccount
       (apats,_) = parsePatternArgs args
       matchdisplayopt Nothing _ = True
       matchdisplayopt (Just e) t = (fromparse $ parsewith datedisplayexpr e) t
