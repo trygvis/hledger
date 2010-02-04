f413da0e76c16c34f2da198a8630c80ca78ef9e7
Simon Michael <simon@joyful.com>
2010-02-04 21:59:07 +0100

    resolve conflicts (New command 'chart': generate balances pie chart)
    Ignore-this: dbc25c61293aa056bdaec486dcc9c69f
    
    darcs-hash:20100204205907-3c3f9-e3b3e1544f9d96d40208f5cae1efc99e4183e6b0.gz

--- Commands/Chart.hs
+++ Commands/Chart.hs
@@ -50,7 +50,7 @@ genPie opts _ l = defaultPieLayout
       items = mapMaybe (uncurry accountPieItem) $
               flatten $
               balances $
-              ledgerAccountTree (depthFromOpts opts) l
+              ledgerAccountTree (fromMaybe 99999 $ depthFromOpts opts) $ cacheLedger' l
 
 -- | Convert all quantities of MixedAccount to a single commodity
 amountValue :: MixedAmount -> Double
