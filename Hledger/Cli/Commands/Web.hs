c7880cd86ee9decfc8b04912ccf17b7162bb9e13
Simon Michael <simon@joyful.com>
2010-09-05 20:18:50 +0200

    fix a failing test, handle unicode better in errors
    Ignore-this: 64c62d72b6882b45aaffe6df855ee7a6
    
    darcs-hash:20100905181850-3c3f9-462f1bdb4240b25fc8943e65eb7dd4cfded4e604.gz

--- Hledger/Cli/Commands/Web.hs
+++ Hledger/Cli/Commands/Web.hs
@@ -736,7 +736,7 @@ hdstringlist :: [String] -> HamletData HledgerWebAppRoute
 hdstringlist ss = HDList [ [([], hdstring s)] | s <- ss ]
 
 instance Failure HamletException Handler
-    where failure = error . show
+    where failure = error' . show
 
 renderHamletFile :: FilePath -> HamletMap HledgerWebAppRoute -> Handler Html
 renderHamletFile hfile hmap = do
