84918480c6e3ca3f4ef8d8ddf1228f011a0e39bd
Simon Michael <simon@joyful.com>
2010-12-27 21:26:22 +0100

    reorganize tests
    Ignore-this: ee3beb4eb4f1c3751622531435971dff
    
    darcs-hash:20101227202622-3c3f9-c9c73e46dff85c16c367856b3e8c5f6884f50932.gz

--- Hledger/Hledger/Cli/Convert.hs
+++ Hledger/Hledger/Cli/Convert.hs
@@ -362,7 +362,7 @@ identify rules defacct desc | null matchingrules = (defacct,desc)
 
 caseinsensitive = ("(?i)"++)
 
-tests_Convert = TestList [
+tests_Hledger_Cli_Convert = TestList [
 
    "convert rules parsing: empty file" ~: do
      -- let assertMixedAmountParse parseresult mixedamount =
